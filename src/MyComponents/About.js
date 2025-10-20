7ee4b1ca71b45b5ed24e59dad559a8c9f8f19ab2
Bilal-Ahmed35 <bilalahmedshah35@gmail.com>
2025-10-21 01:02:50 +0500

    addind React contxt API

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
 export const About = () => {
-    return (
-        <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
-        </div>
-    )
-}
+  const a = useContext(NoteContext);
+  return (
+    <div>
+      <b>
+        This is about {a.name} and he is in {a.class}
+      </b>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis
+        odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam
+        possimus. Inventore vitae, perferendis harum quas cupiditate adipisci
+        repellendus tempore asperiores at consequatur quos?
+      </p>
+    </div>
+  );
+};
